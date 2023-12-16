3d318cd83f235c1ace9d8563ba3cdca336d8aba7
Mahmoud Ghareeb <mahmoudghareeb11111@gmail.com>
2023-12-17 00:55:59 +0200

    adding backend code

--- app.py
+++ app.py
@@ -3,10 +3,65 @@ from streamlit_custom_chat import ChatContainer
 from streamlit_custom_input import ChatInput
 from key_generator.key_generator import generate
 from helper_functions import set_bg_hack, set_page_container_style, refresh
-  
+import numpy as np
+import pandas as pd
+from openai import OpenAI
+import yaml
+import json
+import os
+from dotenv import load_dotenv
+
+
+config = yaml.load(open('./configs/config.star.yaml', 'r'),
+                   Loader=yaml.FullLoader)
+
+load_dotenv()
+
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI(
+    api_key=API_KEY
+)
+
+df = pd.read_csv('./data/star_questions.csv')
+
+
+def get_evaluation(content: str) -> dict:
+    """
+    Evaluate if the provided answer follows the STAR methodology
+
+    :param content: {'question': the question, 'answer': the answer}
+    :return: {"eval": your detailed evaluation of the answer}
+    """
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        temperature=0,
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system",
+              "content": config['prompts']['evaluation_prompt']},
+            {"role": "user", "content": content}
+        ]
+    )
+
+    return json.loads(response.choices[0].message.content)
+
+
+def get_random_question():
+    """
+    get a random question from the csv file
+
+    :return: question 
+    """
+    random_question_idx = np.random.randint(0, 60)
+    data = df.iloc[random_question_idx]
+
+    return data['Question']
+
+
 st.set_page_config(layout="wide")
-  
-set_bg_hack("assets/images/pastel3.jpg") 
+
+set_bg_hack("assets/images/pastel3.jpg")
 
 set_page_container_style()
 
@@ -14,45 +69,60 @@ set_page_container_style()
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-#after getting response from bot, add it to session and refresh
+# after getting response from bot, add it to session and refresh
+
+
 def messageFromChatBot(message):
     key = generate()
-    st.session_state.messages.append({"role":"assistant","content":message,"key":"assistant-"+key.get_key()})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": message, "key": "assistant-"+key.get_key()})
     refresh('assistant')
-    
+
 # st.sidebar.header("header")
 # st.sidebar.subheader(‘1.please chose which app you want to operate’)
 
-#if there are no messages in the session add this one
-if len(st.session_state.messages) ==0:
-    st.session_state.messages.append({"role":"assistant","content":"Hello! How may I help you?","key":0})
-   
-col1, col2 = st.columns([2,13]) 
-        
+
+new_question = get_random_question()
+
+# if there are no messages in the session add this one
+if len(st.session_state.messages) == 0:
+    st.session_state.messages.append(
+        {"role": "assistant", "content": config['openning_message'], "key": 0})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": new_question, "key": 1})
+
+col1, col2 = st.columns([2, 13])
+
 with col1:
     st.markdown("#")
     st.markdown("#")
     st.markdown("#")
-    #button to redirect to quiz page (currently does nothing)
+    # button to redirect to quiz page (currently does nothing)
     st.button("Take a Quiz", use_container_width=True)
-    
+
 with col2:
-    #component that displays the messages
+    # component that displays the messages
     ChatContainer(messages=st.session_state.messages, key="chatcontainer")
 
     # if st.button(label="assistant", key="assistant"):
     #     key = generate()
     #     st.session_state.messages.append({"role": "assistant", "content": "whatever","key":"assistant-"+key.get_key()})
     #     refresh('assistant')
-        
 
-    if prompt:= ChatInput(initialValue="",key="inputButton"):
+    if answer := ChatInput(initialValue="", key="inputButton"):
         key = generate()
-       
+
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt,"key":"user-"+key.get_key()})
-        # st.session_state.messages.append({"role": "assistant", "content": prompt,"key":"assistant-"+key.get_key()})
-        
-        #refresh to update ui with new message
+        st.session_state.messages.append(
+            {"role": "user", "content": answer, "key": "user-"+key.get_key()})
+
+        content = '{question: '+new_question+', answer: '+answer+'}'
+        evaluation = get_evaluation(content)
+        st.session_state.messages.append(
+            {"role": "assistant", "content": evaluation['eval'], "key": "assistant-"+key.get_key()})
+
+        new_question = get_random_question()
+        st.session_state.messages.append(
+            {"role": "assistant", "content": new_question, "key": "assistant-"+key.get_key()})
+
         refresh('inputButton')
-        
